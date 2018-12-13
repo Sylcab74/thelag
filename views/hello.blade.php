6f5f28741f178b70ba46f7af3708c9fa6e558efc
tvililax <thomasf.dacosta@gmail.com>
2018-12-13 14:30:59 +0100

    :lipstick: modif vue game et css

--- views/hello.blade.php
+++ views/hello.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.test')
 
-@section('title', 'Le titre')
+@section('title', 'The Lag - Jeux')
 
 
 @section('content')
-
+    <h1>JEUX</h1>
     <div class="box">
         <ul>
         @forelse ($games as $game)
-            <li>{{ $game->name }} -{{$game->type}}</li>
+            <li class="li_game"><img class='game' src={{ $game->picture}} /> <br> {{ $game->name }} - {{$game->type}} </li>
         @empty
             <p>No users</p>
         @endforelse
