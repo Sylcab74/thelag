e14b93282bc04b832cd81e75fe274134f4f760fd
scabiati <cabiati.sylvain@outlook.com>
2018-12-19 00:28:55 +0100

    :ambulance: change models table_name var to static and update core/Table.class.php to handle static table_name

--- models/User.class.php
+++ models/User.class.php
@@ -6,7 +6,7 @@ use \Lag\Core\Table;
 
 class User extends Table
 {
-    protected $table_name = 'users';
+    protected static $table_name = 'users';
     protected $fields_list = ['id','login', 'password', 'email', 'firstname', 'lastname', 'picture', 'availability'];
 
     public $id;
@@ -22,14 +22,14 @@ class User extends Table
     public function addGame($gameId)
     {
         $query = "INSERT INTO link_games_users ( users_id, games_id ) VALUES ( " . $this->id . ", " . $gameId . " )";
-        
+
         return $this->myQuery($query);
     }
 
     public function removeGame($gameId)
     {
         $query = "DELETE FROM link_games_users WHERE users_id = ". $this->id ." AND games_id = " . $gameId;
-        
+
         return $this->myQuery($query);
     }
 
