f0b2e3e25063add7fe7aaa887203e7723f8b249e
tvililax <thomasf.dacosta@gmail.com>
2018-12-19 22:27:17 +0100

    :lipstick: display menu

--- views/layouts/test.blade.php
+++ views/layouts/test.blade.php
@@ -8,6 +8,15 @@
 <body>
 
 <div class="container">
+    <div id="main-menu" role="nav">
+        <a href="/"><img src="../../public/img/logo.png" alt="The Lag"/></a>
+        <ul>
+            <a href="#"><li>COURS</li></a>
+            <a href="/coach"><li>COACHS</li></a>
+            <a href="/index"><li>JEUX</li></a>
+            <a href="#"><li>CONTACT</li></a>
+        </ul>
+    </div>
     @yield('content')
 </div>
 @yield('javascript')
