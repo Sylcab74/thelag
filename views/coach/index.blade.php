1abb56d773b784672d3b071ae3de12b20a1f8a0a
JrBour <calaruni@hotmail.com>
2018-12-20 09:50:12 +0100

    :construction: Search coach

--- views/coach/index.blade.php
+++ views/coach/index.blade.php
@@ -4,6 +4,8 @@
 
 
 @section('content')
+    <h2>Les coachs</h2>
+    <input type="text" id="search_coachs">
     <div class="box">
         <ul>
             @forelse ($users as $user)
@@ -13,4 +15,53 @@
             @endforelse
         </ul>
     </div>
+@endsection
+
+@section('javascript')
+    <script>
+        document.addEventListener('DOMContentLoaded', () => {
+            const searchInput = document.querySelector('#search_coachs');
+            const list = document.querySelector('.box ul');
+
+            const searchGames =  async e => {
+                e.preventDefault();
+                if (e.keyCode === 13) {
+                    const formData = new FormData();
+                    formData.append('search', searchInput.value);
+
+                    try {
+                        const response = await fetch(window.location.origin + '/coach/search', {
+                            method: 'POST',
+                            mode:"cors",
+                            body : formData
+                        });
+
+                        if (response.ok) {
+                            const data = await response.json();
+                            list.innerHTML = '';
+                            data.response.forEach(user => {
+                                /*list.innerHTML += `
+                                    <a href="game/show/${.id}">
+                                        <li class="li_game">
+                                            <img class="img_game" src="${ game.picture }" alt="${ game.name }" /> <br>
+                                            <h2>${game.name}</h2>
+                                            <p>${game.type}</p>
+                                        </li>
+                                    </a>
+                                `;*/
+                            })
+
+                        } else {
+                            console.error(response.status);
+                        }
+                    } catch(e) {
+                        console.error(e);
+                    }
+                }
+            };
+
+
+            searchInput.addEventListener('keyup', e => searchGames(e))
+        })
+    </script>
 @endsection
