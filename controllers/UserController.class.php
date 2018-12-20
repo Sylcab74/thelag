2b4afc9c9301b36b34fab02585b5b4c5cb28f92b
scabiati <cabiati.sylvain@outlook.com>
2018-12-20 11:35:56 +0100

    WIP authentification, login, register

--- controllers/UserController.class.php
+++ controllers/UserController.class.php
@@ -19,4 +19,24 @@ class UserController
         $data['status'] = 'success';
         $data['response'] = 'Le jeu a bien était ajouté à votre bibliothéque !';
     }
+
+    public function registerAction($params)
+    {
+        $post = $params['POST'];
+        $errors = null;
+
+        if (!empty( User::findBy('login', $post['login'])))
+            $errors[] = "Désolé ce nom d'utilisateur est déjà utilisé.";
+
+        if(!empty(User::findBy('email', $post['email'])))
+            $errors[] = "Désolé cet email est déjà utilisé";
+
+        if( strlen($post['password']) < 8)
+            $errors[] = "Désolé, votre mot de passe doit contenir plus de 8 caractères";
+
+        if(!is_null($errors))
+            return Views::render('user.register', ["errors" => $errors]);
+
+
+    }
 }
